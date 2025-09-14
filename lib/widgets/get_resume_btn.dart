2b1027af476facfba608e128673aeac9163a0550
priya31-prog <196824624+priya31-prog@users.noreply.github.com>
2025-09-14 12:19:53 +0530

    modified resume download btn

---
+++ lib/widgets/get_resume_btn.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/state_management/bloc_provider.dart';
+import 'package:flutter_application_1/state_management/events.dart';
+import 'package:flutter_application_1/state_management/state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+Widget getDownloadButton(BuildContext context) {
+  return BlocBuilder<PortFolioBloc, PortFolioStates>(
+    builder: (context, state) {
+      bool isResumeDownloaded = state.resumeDownload ?? false;
+
+      return state.isLoadingResume ?? false
+          ? CircularProgressIndicator(
+              color: Theme.of(context).colorScheme.primary,
+              strokeWidth: 2,
+            )
+          : Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: Theme.of(context).colorScheme.primary,
+                shape: BoxShape.rectangle,
+              ),
+              child: InkWell(
+                onTap: () {
+                  // if (!isResumeDownloaded) {
+                  context.read<PortFolioBloc>().add(LoadData());
+                  // }
+                },
+                child: !isResumeDownloaded
+                    ? Text(
+                        'Download Resume',
+                        style: TextStyle(fontSize: 12, color: Colors.white),
+                      )
+                    : Text(
+                        'Downloaded',
+                        style: TextStyle(fontSize: 12, color: Colors.white),
+                      ),
+              ),
+            );
+    },
+  );
+}
