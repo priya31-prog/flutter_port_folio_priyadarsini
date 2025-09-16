b238a1e0c59dab7625ed1e27768c9385440e4586
priya31-prog <196824624+priya31-prog@users.noreply.github.com>
2025-09-16 08:44:21 +0530

    80 % about journey

--- lib/Screens/about_journey.dart
+++ lib/Screens/about_journey.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/high_light_text.dart';
 
 class AboutJourney extends StatefulWidget {
   const AboutJourney({super.key});
@@ -8,19 +9,6 @@ class AboutJourney extends StatefulWidget {
 }
 
 class _AboutJourney extends State<AboutJourney> {
-  final List<IconData> icons = [
-    Icons.school_outlined,
-    Icons.shopping_cart_outlined,
-    Icons.payment_outlined,
-  ];
-
-  final List<String> year = ['2018-2022', '2022-2023', 'Current'];
-
-  final List<String> title = [
-    'Anna University',
-    'Worked on Tata Neu Super App',
-    'Contributing to SBI YONO (UPI module)',
-  ];
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -31,40 +19,102 @@ class _AboutJourney extends State<AboutJourney> {
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
         ),
         SizedBox(height: 20),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              child: ListView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: 3,
-                itemBuilder: (context, index) {
-                  return buildAbout(
-                    context: context,
-                    icon: icons[index],
-                    title: title[index],
-                    year: year[index],
-                  );
-                },
-              ),
-            ),
-
-            Expanded(
-              child: Column(
-                children: [
-                  Text(
-                    'In today’s fast-paced digital world, technology continues to evolve at an incredible rate, shaping the way people communicate, work, and solve problems. Mobile applications have become a central part of daily life, offering convenience, speed, and endless possibilities for businesses and individuals alike. Developers play a key role in this transformation, combining creativity with technical skills to build solutions that truly make an impact. From designing user-friendly interfaces to integrating powerful backend systems, every step requires precision and innovation. The journey of learning, experimenting, and improving never ends, making software development both challenging and deeply rewarding.',
-                  ),
-                ],
-              ),
-            ),
-          ],
+        LayoutBuilder(
+          builder: (context, constraints) {
+            bool isSmallScreen = constraints.maxWidth < 500;
+            if (isSmallScreen) {
+              return Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [timeLine(context), buildText(context)],
+                ),
+              );
+            } else {
+              return Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(child: timeLine(context)),
+                    SizedBox(width: 20),
+                    Expanded(child: buildText(context)),
+                  ],
+                ),
+              );
+            }
+          },
         ),
       ],
     );
   }
+}
+
+Widget buildText(BuildContext context) {
+  return highlightedText(
+    context: context,
+    text:
+        'I’m a Flutter Developer with 3+ years of experience building fintech and lifestyle applications that impact millions of users.\n\nAt Tata Consultancy Services, I’ve contributed to flagship apps like SBI YONO and Tata Neu, delivering complex payment modules, enhancing app performance, and resolving critical defects with a strong focus on UI/UX, security, and reliability.\n\nI thrive in Agile environments, collaborating with teams across development, QA, and product to deliver scalable and high-quality mobile solutions.\n\n I’m passionate about creating seamless, user-friendly digital experiences while continuously learning and refining my craft.',
+    boldWords: [
+      'Flutter Developer',
+      '3+ years',
+      'Tata Consultancy',
+      'SBI YONO',
+      'Tata Neu',
+      'UI/UX',
+      'Agile environments',
+    ],
+  );
+  // return RichText(
+  //   // textAlign: TextAlign.center,
+  //   text: TextSpan(
+  //     style: Theme.of(context).textTheme.bodySmall,
+
+  //     children: [
+  //       TextSpan(
+  //         text:
+  //             'I’m a Flutter Developer with 3+ years of experience building fintech and lifestyle applications that impact millions of users. At Tata Consultancy Services, I’ve contributed to flagship apps like SBI YONO and Tata Neu, delivering complex payment modules, enhancing app performance, and resolving critical defects with a strong focus on UI/UX, security, and reliability.\n\n',
+  //       ),
+
+  //       TextSpan(
+  //         text:
+  //             ' I specialize in Flutter, Dart, MobX, Riverpod, and REST APIs, with hands-on expertise in cross-platform mobile development, state management, and payment integrations (UPI, cards, merchant solutions). I thrive in Agile environments, collaborating with teams across development, QA, and product to deliver scalable and high-quality mobile solutions.\n\n',
+  //       ),
+  //       TextSpan(
+  //         text:
+  //             'Recognized as a Star Performer (2024) for resolving 40% of critical application bugs, I’m passionate about creating seamless, user-friendly digital experiences while continuously learning and refining my craft.\n\n',
+  //       ),
+  //     ],
+  //   ),
+  // );
+}
+
+Widget timeLine(BuildContext context) {
+  final List<IconData> icons = [
+    Icons.school_outlined,
+    Icons.shopping_cart_outlined,
+    Icons.payment_outlined,
+  ];
+
+  final List<String> year = ['2018-2022', '2022-2023 - TCS', 'Current - TCS'];
+
+  final List<String> title = [
+    'Anna University',
+    'Worked on Tata Neu Super App',
+    'Contributing to SBI YONO (UPI module)',
+  ];
+  return ListView.builder(
+    shrinkWrap: true,
+    physics: NeverScrollableScrollPhysics(),
+    itemCount: 3,
+    itemBuilder: (context, index) {
+      return buildAbout(
+        context: context,
+        icon: icons[index],
+        title: title[index],
+        year: year[index],
+      );
+    },
+  );
 }
 
 Widget buildAbout({
@@ -82,19 +132,24 @@ Widget buildAbout({
           Column(
             children: [
               Icon(icon),
-              Container(height: 50, width: 2, color: Colors.grey),
+              Container(height: 80, width: 2, color: Colors.grey),
             ],
           ),
           SizedBox(width: 15),
-          Padding(
-            padding: EdgeInsetsGeometry.symmetric(vertical: 5, horizontal: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(year, style: TextStyle(fontWeight: FontWeight.bold)),
-                // WidgetSpan(child: SizedBox(height: 10)),
-                Text(title),
-              ],
+          Expanded(
+            child: Padding(
+              padding: EdgeInsetsGeometry.symmetric(
+                vertical: 5,
+                horizontal: 10,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(year, style: TextStyle(fontWeight: FontWeight.bold)),
+                  SizedBox(height: 6),
+                  Text(title, softWrap: true, overflow: TextOverflow.visible),
+                ],
+              ),
             ),
           ),
         ],
