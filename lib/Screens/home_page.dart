c1d6aa60c9a9b709863042d661eb0422ccccc1a1
priya31-prog <196824624+priya31-prog@users.noreply.github.com>
2025-09-16 08:44:44 +0530

    added journey

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Screens/about_journey.dart';
 import 'package:flutter_application_1/Screens/profile_header.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,8 +18,9 @@ class _HomePage extends State<HomePage> {
           padding: EdgeInsets.symmetric(vertical: 24, horizontal: 16),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 30,
             // mainAxisSize: MainAxisSize.min,
-            children: [ProfileHeader()],
+            children: [ProfileHeader(), AboutJourney()],
           ),
         ),
       ),
