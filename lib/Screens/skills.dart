047f96b2ea2315c71c6edcf9c2ee6ae8ecebf5c8
priya31-prog <196824624+priya31-prog@users.noreply.github.com>
2025-09-17 07:44:31 +0530

    skills section

--- lib/Screens/skills.dart
+++ lib/Screens/skills.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Screens/state_managements_skills.dart';
 
 class SkillPage extends StatefulWidget {
   const SkillPage({super.key});
@@ -8,6 +9,16 @@ class SkillPage extends StatefulWidget {
 }
 
 class _SkillPage extends State<SkillPage> {
+  final List<String> skills = [
+    'Flutter',
+    'Dart',
+    'Python',
+    'Django Rest Framework',
+    'GitHub',
+    'Firebase',
+  ];
+
+  final List<double> skillsPercent = [0.8, 0.9, 0.8, 0.7, 0.7, 0.6];
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -15,37 +26,69 @@ class _SkillPage extends State<SkillPage> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Skills'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Card(
-                elevation: 30,
+          Text(
+            'Skills',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          GridView.builder(
+            shrinkWrap: true,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20,
+              childAspectRatio: 2,
+              mainAxisExtent: 100,
+            ),
+            itemCount: 6,
+            itemBuilder: (context, index) {
+              return Card(
                 child: Padding(
                   padding: EdgeInsetsGeometry.symmetric(
-                    horizontal: 24,
-                    vertical: 10,
+                    horizontal: 10,
+                    vertical: 8,
                   ),
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text('Flutter'),
-                      SizedBox(width: 40, child: LinearProgressIndicator()),
+                      Center(
+                        child: Text(
+                          skills[index],
+                          overflow: TextOverflow.visible,
+                          softWrap: true,
+                          textAlign: TextAlign.center,
+                          maxLines: 2,
+                        ),
+                      ),
+
+                      SizedBox(height: 5),
+                      TweenAnimationBuilder<double>(
+                        tween: Tween<double>(
+                          begin: 0,
+                          end: skillsPercent[index],
+                        ),
+                        duration: const Duration(seconds: 5),
+                        builder: (context, value, _) {
+                          return LinearProgressIndicator(
+                            value: value,
+                            minHeight: 5,
+                          );
+                        },
+                      ),
                     ],
                   ),
                 ),
-              ),
-              Text('Dart'),
-              Text('Python'),
-            ],
+              );
+            },
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [Text('Flutter'), Text('Dart'), Text('Python')],
+          SizedBox(height: 20),
+          Text(
+            'State Management',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [Text('Flutter'), Text('Dart'), Text('Python')],
-          ),
+          SizedBox(height: 20),
+
+          stateManagement(context),
         ],
       ),
     );
