5096a81c6e150a73cd2bcc70c3762c9ec7f75e54
priya31-prog <196824624+priya31-prog@users.noreply.github.com>
2025-09-17 08:10:12 +0530

    skills

--- lib/Screens/skills.dart
+++ lib/Screens/skills.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/Screens/state_managements_skills.dart';
+import 'package:flutter_application_1/constants.dart';
+import 'package:flutter_application_1/widgets/skill_gridview.dart';
 
 class SkillPage extends StatefulWidget {
   const SkillPage({super.key});
@@ -9,86 +10,40 @@ class SkillPage extends StatefulWidget {
 }
 
 class _SkillPage extends State<SkillPage> {
-  final List<String> skills = [
-    'Flutter',
-    'Dart',
-    'Python',
-    'Django Rest Framework',
-    'GitHub',
-    'Firebase',
-  ];
-
-  final List<double> skillsPercent = [0.8, 0.9, 0.8, 0.7, 0.7, 0.6];
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsetsGeometry.symmetric(horizontal: 10, vertical: 24),
       child: Column(
+        spacing: 20,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
             'Skills',
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
-          SizedBox(height: 20),
-          GridView.builder(
-            shrinkWrap: true,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20,
-              childAspectRatio: 2,
-              mainAxisExtent: 100,
-            ),
-            itemCount: 6,
-            itemBuilder: (context, index) {
-              return Card(
-                child: Padding(
-                  padding: EdgeInsetsGeometry.symmetric(
-                    horizontal: 10,
-                    vertical: 8,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Center(
-                        child: Text(
-                          skills[index],
-                          overflow: TextOverflow.visible,
-                          softWrap: true,
-                          textAlign: TextAlign.center,
-                          maxLines: 2,
-                        ),
-                      ),
 
-                      SizedBox(height: 5),
-                      TweenAnimationBuilder<double>(
-                        tween: Tween<double>(
-                          begin: 0,
-                          end: skillsPercent[index],
-                        ),
-                        duration: const Duration(seconds: 5),
-                        builder: (context, value, _) {
-                          return LinearProgressIndicator(
-                            value: value,
-                            minHeight: 5,
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
+          skillGridView(
+            context: context,
+            items: AppConstants().skills,
+            percentages: AppConstants().skillsPercent,
           ),
-          SizedBox(height: 20),
+
           Text(
             'State Management',
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
-          SizedBox(height: 20),
 
-          stateManagement(context),
+          skillGridView(
+            context: context,
+            items: AppConstants().stateManagement,
+          ),
+          Text(
+            'Tools and PlatForms',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+
+          skillGridView(context: context, items: AppConstants().toolsPlatForms),
         ],
       ),
     );
---
+++ lib/constants.dart
@@ -0,0 +1,24 @@
+class AppConstants {
+  final List<String> skills = [
+    'Flutter',
+    'Dart',
+    'Python',
+    'Django Rest Framework',
+    'GitHub',
+    'Firebase',
+    'Javascript',
+    'HMTL & CSS',
+  ];
+
+  final List<String> stateManagement = ['Mobx', 'RiverPod', 'Bloc'];
+  final List<double> skillsPercent = [0.9, 0.9, 0.8, 0.7, 0.7, 0.6, 0.6, 0.7];
+
+  final List<String> toolsPlatForms = [
+    'VS Code',
+    'Andriod Studio',
+    'Postman',
+    'MasterCraft',
+    'GitLab',
+    'GitHub',
+  ];
+}
