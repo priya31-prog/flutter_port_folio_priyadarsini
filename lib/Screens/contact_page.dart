a0f4f2396decdd08fe5b7e65006bb286b9876349
priya31-prog <196824624+priya31-prog@users.noreply.github.com>
2025-09-23 08:30:28 +0530

    contact page

---
+++ lib/Screens/contact_page.dart
@@ -0,0 +1,151 @@
+// import 'dart:nativewrappers/_internal/vm/lib/developer.dart';
+
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+// import 'package:flutter/widgets.dart';
+
+class ContactPage extends StatelessWidget {
+  const ContactPage({super.key});
+
+  Future<void> _launchUrl({required String urlName}) async {
+    final Uri url = Uri.parse(urlName);
+    if (!await launchUrl(url)) {
+      log('Failed to launch url');
+    }
+  }
+
+  Future<void> _launchMail(String mail) async {
+    final Uri emailUri = Uri(
+      scheme: 'mailto',
+      path: 'priyadarsinik31@gmail.com',
+      query: 'Regarding recruitment',
+    );
+
+    if (await canLaunchUrl(emailUri)) {
+      await launchUrl(emailUri);
+    } else {
+      log('Error in launching email url');
+    }
+  }
+
+  Future<void> _launchDialer(String mobile) async {
+    final Uri phoneUrl = Uri(scheme: 'tel', path: mobile);
+    if (await canLaunchUrl(phoneUrl)) {
+      launchUrl(phoneUrl);
+    } else {
+      log('Error in launching url');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsetsGeometry.symmetric(horizontal: 10, vertical: 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Get in Touch',
+            style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+
+          SizedBox(height: 15),
+          Column(
+            children: [
+              Text(
+                "I'd like to here from you",
+                style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 13,
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.mail_outline),
+                  SizedBox(width: 15),
+                  TextButton(
+                    onPressed: () {
+                      _launchMail('priyadarsinik31@gmail.com');
+                    },
+                    child: Text(
+                      'priyadarsinik31@gmail.com',
+                      style: TextStyle(decoration: TextDecoration.underline),
+                    ),
+                  ),
+                ],
+              ),
+
+              // SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 10,
+                children: [
+                  Icon(Icons.thumb_up_alt_outlined),
+                  SizedBox(width: 5),
+                  InkWell(
+                    onTap: () {
+                      _launchUrl(
+                        urlName:
+                            'https://www.hackerrank.com/profile/priyadarsinik31',
+                      );
+                    },
+                    child: SizedBox(
+                      height: 50,
+                      width: 30,
+                      child: Image.asset('images/hacker_rank_logo.png'),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      _launchUrl(
+                        urlName:
+                            'https://www.linkedin.com/in/priyadarsini-k-b74832213/',
+                      );
+                    },
+                    child: SizedBox(
+                      height: 30,
+                      width: 30,
+                      child: Image.asset('images/linked_in_logo.png'),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      _launchUrl(urlName: 'https://github.com/priya31-prog');
+                    },
+                    child: SizedBox(
+                      height: 20,
+                      width: 30,
+                      child: Image.asset('images/git_hub_logo.jpg'),
+                    ),
+                  ),
+                ],
+              ),
+
+              // SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.call),
+                  SizedBox(width: 15),
+                  TextButton(
+                    onPressed: () {
+                      _launchDialer('+917708309962');
+                    },
+                    child: Text('+91 7708309962'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
