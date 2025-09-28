5d967d327e574f939215c5c39d7276cab192c3f4
priya31-prog <196824624+priya31-prog@users.noreply.github.com>
2025-09-28 20:14:02 +0530

    app name change

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flutter_application_1"
+    namespace = "com.example.Portfolio-Priyadarsini"
     compileSdk = flutter.compileSdkVersion
     ndkVersion =  "27.0.12077973"
 
